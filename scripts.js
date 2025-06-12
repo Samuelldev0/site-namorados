56b5bbc213d8b6cce3c7f99f17e4f61d440693db
Samuelldev0 <ss992634221@gmail.com>
2025-06-11 22:13:38 -0300

    presentinho-pronto

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
 function mostrarConteudo() {
-      document.getElementById("conteudo").style.display = "block";
-      const musica = document.getElementById("musica");
-      musica.play().catch(e => {
+    document.getElementById("conteudo").style.display = "block";
+    const musica = document.getElementById("musica");
+    musica.play().catch(e => {
         console.log("Autoplay bloqueado. Usuário precisa interagir com a página.");
-      });
-    }
+    });
+
+    iniciarSlideshow();
+}
+
+function iniciarSlideshow() {
+    const slides = document.querySelectorAll('.slide');
+    let index = 0;
+
+    slides[index].classList.add('ativo');
+
+    setInterval(() => {
+        slides[index].classList.remove('ativo');
+        index = (index + 1) % slides.length;
+        slides[index].classList.add('ativo');
+    }, 2000); 
+}
